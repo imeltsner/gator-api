87ee8eeee5c767b9123977912f705847fe2241bb
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-10-14 16:21:21 -0700

    added main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/imeltser/gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		return
+	}
+
+	err = cfg.SetUser("isaac")
+	if err != nil {
+		return
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("DB is: %v\nUser is %v\n", cfg.DBURL, cfg.CurrentUsername)
+}

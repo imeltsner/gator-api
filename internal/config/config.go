873ae6596c428638f9431e5233fb9238e6aa47d2
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-10-14 16:06:29 -0700

    added config functions

---
+++ internal/config/config.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DBURL           string `json:"db_url"`
+	CurrentUsername string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	configFile, err := os.Open(configFilePath)
+	if err != nil {
+		return Config{}, fmt.Errorf("unable to open config file at path %v: %v", configFileName, err)
+	}
+	defer configFile.Close()
+
+	var cfg Config
+	reader := bufio.NewReader(configFile)
+	decoder := json.NewDecoder(reader)
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return Config{}, fmt.Errorf("unable to decode config file: %v", err)
+	}
+
+	return cfg, nil
+}
+
+func getConfigFilePath() (string, error) {
+	homePath, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("unable to get path to home dir: %v", err)
+	}
+
+	return filepath.Join(homePath, configFileName), nil
+}
+
+func (cfg *Config) SetUser(user string) error {
+	cfg.CurrentUsername = user
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	configFile, err := os.Create(configFilePath)
+	if err != nil {
+		return fmt.Errorf("unable to create config file: %v", err)
+	}
+	defer configFile.Close()
+
+	encoder := json.NewEncoder(configFile)
+	err = encoder.Encode(cfg)
+	if err != nil {
+		return fmt.Errorf("unable to write json to config file %v", err)
+	}
+
+	return nil
+}

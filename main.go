08af2bed0beaff77784b5aa6289e64306c20b898
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-10-14 17:32:39 -0700

    added login command

--- main.go
+++ main.go
@@ -2,25 +2,45 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/imeltsner/gator/internal/config"
 )
 
 func main() {
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Printf("program expects at least 1 arg\n")
+		os.Exit(1)
+	}
+
 	cfg, err := config.Read()
 	if err != nil {
-		return
+		os.Exit(1)
 	}
 
-	err = cfg.SetUser("isaac")
+	s := state{
+		cfg: &cfg,
+	}
+
+	cmds := commands{
+		cmds: map[string]func(*state, command) error{},
+	}
+	cmds.register("login", handlerLogin)
+
+	cmdName := args[1]
+	var cmdSubArgs []string
+	if len(args) > 2 {
+		cmdSubArgs = args[2:]
+	}
+
+	cmd := command{
+		name: cmdName,
+		args: cmdSubArgs,
+	}
+
+	err = cmds.run(&s, cmd)
 	if err != nil {
-		return
+		os.Exit(1)
 	}
-
-	cfg, err = config.Read()
-	if err != nil {
-		return
-	}
-
-	fmt.Printf("DB is: %v\nUser is %v\n", cfg.DBURL, cfg.CurrentUsername)
 }

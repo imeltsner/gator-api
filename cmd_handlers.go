6b83b5cac2f9caf611b93a98588a70e160f0d5e6
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-10-15 22:28:06 -0700

    added register handler and updated login handler

--- cmd_handlers.go
+++ cmd_handlers.go
@@ -1,17 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/imeltsner/gator/internal/database"
+)
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) != 1 {
 		return fmt.Errorf("login command expects 1 argument")
 	}
 
-	err := s.cfg.SetUser(cmd.args[0])
+	dbUser, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("unable to login: %v", err)
+	}
+
+	err = s.cfg.SetUser(dbUser.Name)
 	if err != nil {
 		return err
 	}
 
 	fmt.Printf("User has been set to %v\n", cmd.args[0])
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("register command expects 1 argument")
+	}
+
+	user := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      cmd.args[0],
+	}
+
+	dbUser, err := s.db.CreateUser(context.Background(), user)
+	if err != nil {
+		return fmt.Errorf("unable to create db user: %v", err)
+	}
+
+	err = s.cfg.SetUser(cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("unable to set user: %v", err)
+	}
+
+	fmt.Printf("DB user with name %v and ID %v created at %v\n", dbUser.Name, dbUser.ID, dbUser.CreatedAt)
 	return nil
 }

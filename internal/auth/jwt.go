5cb8b8266bc08acc91e74e1979fe4460a543abee
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-10-24 19:12:56 -0700

    makeJWT and ValidateJWT

---
+++ internal/auth/jwt.go
@@ -0,0 +1,57 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "gatorapi-access",
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+
+	signedString, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", fmt.Errorf("unable to sign string %v", err)
+	}
+
+	return signedString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&claims,
+		func(token *jwt.Token) (interface{}, error) { return []byte(tokenSecret), nil },
+	)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("unable to parse claims: %v", err)
+	}
+
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("unable to get issuer: %v", err)
+	}
+	if issuer != "gatorapi-access" {
+		return uuid.Nil, fmt.Errorf("bad issuer: %v", issuer)
+	}
+
+	idString, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("unable to get subject: %v", err)
+	}
+
+	id, err := uuid.Parse(idString)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("unable to parse id string: %v", err)
+	}
+
+	return id, nil
+}

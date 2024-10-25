843c40d383f4bc61138e1305b8d7e23ac18cd1a6
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-10-24 18:13:35 -0700

    hashed password column

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -13,21 +13,23 @@ import (
 )
 
 const createUser = `-- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, name)
+INSERT INTO users (id, created_at, updated_at, name, hashed_password)
 VALUES (
     $1,
     $2,
     $3,
-    $4
+    $4,
+    $5
 )
-RETURNING id, created_at, updated_at, name
+RETURNING id, created_at, updated_at, name, hashed_password
 `
 
 type CreateUserParams struct {
-	ID        uuid.UUID
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Name      string
+	ID             uuid.UUID
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	Name           string
+	HashedPassword string
 }
 
 func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, error) {
@@ -36,6 +38,7 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 		arg.CreatedAt,
 		arg.UpdatedAt,
 		arg.Name,
+		arg.HashedPassword,
 	)
 	var i User
 	err := row.Scan(
@@ -43,6 +46,7 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 		&i.CreatedAt,
 		&i.UpdatedAt,
 		&i.Name,
+		&i.HashedPassword,
 	)
 	return i, err
 }
@@ -67,7 +71,7 @@ func (q *Queries) DeleteUsers(ctx context.Context) error {
 }
 
 const getUserByID = `-- name: GetUserByID :one
-SELECT id, created_at, updated_at, name FROM users
+SELECT id, created_at, updated_at, name, hashed_password FROM users
 WHERE id = $1
 `
 
@@ -79,12 +83,13 @@ func (q *Queries) GetUserByID(ctx context.Context, id uuid.UUID) (User, error) {
 		&i.CreatedAt,
 		&i.UpdatedAt,
 		&i.Name,
+		&i.HashedPassword,
 	)
 	return i, err
 }
 
 const getUserByName = `-- name: GetUserByName :one
-SELECT id, created_at, updated_at, name FROM users
+SELECT id, created_at, updated_at, name, hashed_password FROM users
 WHERE name = $1
 `
 
@@ -96,6 +101,7 @@ func (q *Queries) GetUserByName(ctx context.Context, name string) (User, error) 
 		&i.CreatedAt,
 		&i.UpdatedAt,
 		&i.Name,
+		&i.HashedPassword,
 	)
 	return i, err
 }
@@ -113,7 +119,7 @@ func (q *Queries) GetUserNameByID(ctx context.Context, id uuid.UUID) (string, er
 }
 
 const getUsers = `-- name: GetUsers :many
-SELECT id, created_at, updated_at, name FROM users
+SELECT id, created_at, updated_at, name, hashed_password FROM users
 `
 
 func (q *Queries) GetUsers(ctx context.Context) ([]User, error) {
@@ -130,6 +136,7 @@ func (q *Queries) GetUsers(ctx context.Context) ([]User, error) {
 			&i.CreatedAt,
 			&i.UpdatedAt,
 			&i.Name,
+			&i.HashedPassword,
 		); err != nil {
 			return nil, err
 		}
